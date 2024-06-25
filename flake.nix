b59fb59cb80ec1d4822d6e7dbe854f08d81ccae6
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-25 16:47:17 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      libraries = with pkgs; [
+        webkitgtk_4_1.dev
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+        libsoup
+      ];
+
+      packages = with pkgs; [
+        webkitgtk_4_1.dev
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        librsvg
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = packages;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+        '';
+      };
+    });
+}
