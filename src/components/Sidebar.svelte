9317f7688340ca1880c3cf7c612f26f0148656a1
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-25 18:52:33 +0200

    feat: added frontend boilerplate and initial window

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import { Button} from '../lib';
+let open = $state(true);
+
+    function toggleOpen() {
+        open = !open;
+    }
+</script>
+
+<aside class={open ? 'open' : ''}>
+    <div class="handle">
+        <Button onclick={toggleOpen}>
+            {#if open}
+                ≪
+            {:else}
+                ≫
+            {/if}
+        </Button>
+    </div>
+
+    <ul>
+        <li>Home</li>
+    </ul>
+</aside>
+
+
+<style>
+    aside {
+        width: 300px;
+        height: 100vh;
+        background: var(--color-accent);
+        position: fixed;
+        left: -300px;
+
+        color: var(--color-primary);
+
+        display: flex;
+        flex-direction: column;
+
+        box-shadow: 3px 1px 5px 0px rgba(0, 0, 0, 0.75);
+        transition: left 0.5s;
+    }
+
+    aside.open {
+        left: 0;
+    }
+
+    aside ul {
+        padding-inline-start: 0;
+    }
+
+    aside ul li {
+        list-style: none;
+        font-size: 18px;
+        padding: 20px 0;
+        cursor: pointer;
+    }
+
+    .handle {
+        position: relative;
+        left: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,156 +1,101 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+    import { P, H, Button, TextInput } from '../lib';
+    import Sidebar from '../components/Sidebar.svelte';
 
-  let name = "";
-  let greetMsg = "";
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name });
-  }
+    function send() {
+        console.log('send');
+    }
 </script>
 
-<div class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-
-  <p>{greetMsg}</p>
-</div>
+<Sidebar />
+<main>
+    <div class="chat-section">
+        <div class="chat-box">
+            <div class="history">
+                <div class="user-message">
+                    <P>Hi, Alice, what can you do?</P>
+                </div>
+                <div class="bot-message">
+                    <P>Hi, I'm Alice, your personal assistant. I can help you with a lot of things.</P>
+                    <H size="2">Capibilities</H>
+                    <ul>
+                        <li>Search for information on the web.</li>
+                        <li>Help you with your homework.</li>
+                        <li>Write stories.</li>
+                        <li>Help you with your workout plan.</li>
+                        <li>Write code.</li>
+                    </ul>
+                    <H size="2">Examples</H>
+                    <ul>
+                        <li>Create a workout plan.</li>
+                        <li>Help me with my math homework.</li>
+                        <li>Write a story about a dragon.</li>
+                        <li>Write a program that prints "Hello, World!".</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="bottom-bar">
+                <TextInput name="userMessage" placeholder="Chat with Alice." />
+                <Button onclick={send}>Send</Button>
+            </div>
+        </div>
+    </div>
+</main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte-kit:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
-
-  :root {
-    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-    font-size: 16px;
-    line-height: 24px;
-    font-weight: 400;
-
-    color: #0f0f0f;
-    background-color: #f6f6f6;
-
-    font-synthesis: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-text-size-adjust: 100%;
-  }
-
-  .container {
-    margin: 0;
-    padding-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-  }
-
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: 0.75s;
-  }
-
-  .logo.tauri:hover {
-    filter: drop-shadow(0 0 2em #24c8db);
-  }
-
-  .row {
-    display: flex;
-    justify-content: center;
-  }
-
-  a {
-    font-weight: 500;
-    color: #646cff;
-    text-decoration: inherit;
-  }
-
-  a:hover {
-    color: #535bf2;
-  }
-
-  h1 {
-    text-align: center;
-  }
-
-  input,
-  button {
-    border-radius: 8px;
-    border: 1px solid transparent;
-    padding: 0.6em 1.2em;
-    font-size: 1em;
-    font-weight: 500;
-    font-family: inherit;
-    color: #0f0f0f;
-    background-color: #ffffff;
-    transition: border-color 0.25s;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  button:hover {
-    border-color: #396cd8;
-  }
-  button:active {
-    border-color: #396cd8;
-    background-color: #e8e8e8;
-  }
-
-  input,
-  button {
-    outline: none;
-  }
-
-  #greet-input {
-    margin-right: 5px;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    :root {
-      color: #f6f6f6;
-      background-color: #2f2f2f;
+    main {
+        display: flex;
+        height: 100svh;
+        flex-direction: row;
     }
 
-    a:hover {
-      color: #24c8db;
+    .chat-section {
+        display: flex;
+        flex-grow: 1;
+        height: 100%;
+        justify-content: center;
     }
 
-    input,
-    button {
-      color: #ffffff;
-      background-color: #0f0f0f98;
+    .chat-box {
+        display: flex;
+        flex-direction: column;
+        width: max(500px, 50%);
     }
-    button:active {
-      background-color: #0f0f0f69;
+
+    .history {
+        overflow-y: auto;
+        display: flex;
+        flex-direction: column;
     }
-  }
+
+    .bot-message {
+        border: 1px solid var(--color-action);
+        width: 100%;
+    }
+
+    .user-message {
+        background-color: var(--color-accent);
+        padding: 1rem;
+        margin: 1rem;
+        border-radius: 1rem;
+        color: var(--color-accent);
+        align-self: flex-end;
+    }
+
+    .bottom-bar {
+        display: flex;
+        float: bottom;
+        justify-content: center;
+        align-items: center;
+        padding-top: 1rem;
+    }
+
+    .bottom-bar :global(input) {
+        width: 100%;
+        min-width: max(500px, 50%);
+    }
+
+    .bottom-bar :global(button) {
+        margin: 0 0 0 1rem;
+    }
 </style>
