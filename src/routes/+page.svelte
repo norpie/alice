b81064a699e80ee6477f51ecbc6f8b39ff1583b0
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-06-28 00:47:07 +0200

    feat(frontend): added basic completion demo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { P, H, Button, TextInput } from '../lib';
+    import { invoke } from '@tauri-apps/api/core';
+    import { listen } from '@tauri-apps/api/event';
     import Sidebar from '../components/Sidebar.svelte';
     import ConnectionIndicator from '../components/ConnectionIndicator.svelte';
 
@@ -12,9 +14,47 @@
     let status = $state("disconnected");
     status = data.status;
 
-    function send() {
-        console.log('send');
+    interface CompletionTokens {
+        tokens: string
     }
+
+    listen<CompletionTokens>('completion-tokens', (event) => {
+        if (!event.payload || !event.payload.tokens) {
+            return;
+        }
+        // Remove latest bot message
+        let latest = history.pop();
+        if (!latest || latest.role !== "bot") {
+            return;
+        }
+        if (latest.message === "...") {
+            latest.message = "";
+        }
+        history.push({ role: "bot", message: latest.message + event.payload.tokens });
+    });
+
+    async function send() {
+        let inputElement = document.querySelector('input[name="userMessage"]') as HTMLInputElement;
+        if (!inputElement) return;
+        let message = inputElement.value;
+        if (!message) return;
+        history.push({ role: "user", message });
+        inputElement.value = '';
+        history.push({ role: "bot", message: "..." });
+        let completion: string = await invoke("complete", { snippet: message });
+        history.pop();
+        history.push({ role: "bot", message: completion });
+    }
+
+    interface Message {
+        role: 'user' | 'bot',
+        message: string
+    }
+
+    let history: Message[] = $state([{
+        role: "bot",
+        message: "Hi, I'm Alice, your personal assistant. I can help you with a lot of things."
+    }]);
 </script>
 
 <Sidebar />
@@ -22,27 +62,38 @@
     <div class="chat-section">
         <div class="chat-box">
             <div class="history">
-                <div class="user-message">
-                    <P>Hi, Alice, what can you do?</P>
-                </div>
-                <div class="bot-message">
-                    <P>Hi, I'm Alice, your personal assistant. I can help you with a lot of things.</P>
-                    <H size="2">Capibilities</H>
-                    <ul>
-                        <li>Search for information on the web.</li>
-                        <li>Help you with your homework.</li>
-                        <li>Write stories.</li>
-                        <li>Help you with your workout plan.</li>
-                        <li>Write code.</li>
-                    </ul>
-                    <H size="2">Examples</H>
-                    <ul>
-                        <li>Create a workout plan.</li>
-                        <li>Help me with my math homework.</li>
-                        <li>Write a story about a dragon.</li>
-                        <li>Write a program that prints "Hello, World!".</li>
-                    </ul>
-                </div>
+                {#each history as message}
+                    {#if message.role === "user"}
+                        <div class="user-message">
+                            <P>{message.message}</P>
+                        </div>
+                    {:else}
+                        <div class="bot-message">
+                            <P>{message.message}</P>
+                        </div>
+                    {/if}
+                {/each}
+                <!-- <div class="user-message"> -->
+                <!--     <P>Hi, Alice, what can you do?</P> -->
+                <!-- </div> -->
+                <!-- <div class="bot-message"> -->
+                <!--     <P>Hi, I'm Alice, your personal assistant. I can help you with a lot of things.</P> -->
+                <!--     <H size="2">Capibilities</H> -->
+                <!--     <ul> -->
+                <!--         <li>Search for information on the web.</li> -->
+                <!--         <li>Help you with your homework.</li> -->
+                <!--         <li>Write stories.</li> -->
+                <!--         <li>Help you with your workout plan.</li> -->
+                <!--         <li>Write code.</li> -->
+                <!--     </ul> -->
+                <!--     <H size="2">Examples</H> -->
+                <!--     <ul> -->
+                <!--         <li>Create a workout plan.</li> -->
+                <!--         <li>Help me with my math homework.</li> -->
+                <!--         <li>Write a story about a dragon.</li> -->
+                <!--         <li>Write a program that prints "Hello, World!".</li> -->
+                <!--     </ul> -->
+                <!-- </div> -->
             </div>
             <div class="bottom-bar">
                 <ConnectionIndicator bind:status />
