56d0a40ad8b8458849ce128830b059ef98c48f8f
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-15 18:30:53 +0100

    feat: add prelude with error and result

---
+++ src-tauri/src/prelude.rs
@@ -0,0 +1,29 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, AliceError>;
+
+#[derive(Debug, Error)]
+pub enum AliceError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Serde error: {0}")]
+    Serde(#[from] serde_json::Error),
+
+    // OnceLock
+    #[error("OnceLock is empty")]
+    OnceLockEmpty,
+
+    // Handlebars
+    #[error("Handlebars error: {0}")]
+    Handlebars(#[from] handlebars::RenderError),
+
+    // Sockets
+    #[error("Tungstenite error: {0}")]
+    Tungstenite(#[from] tokio_tungstenite::tungstenite::Error),
+    #[error("No stream")]
+    NoStream,
+    #[error("Response error")]
+    ResponseError,
+    #[error("Invalid message")]
+    InvalidMessage,
+}
