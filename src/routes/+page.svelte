d337f18333f1465a2af86ada112b7fd7d7fa5721
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-19 18:37:10 +0100

    refactor: start refactor of chat page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,205 +1,215 @@
 <script lang="ts">
-    import { P, Button, Textarea } from "../lib";
-    import { invoke } from "@tauri-apps/api/core";
-    import { listen } from "@tauri-apps/api/event";
-    import Sidebar from "../components/Sidebar.svelte";
-    import ConnectionIndicator from "../components/ConnectionIndicator.svelte";
-    import { toHighlightedHtml, toHtml } from "../lib/markdown";
+    import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
     import { resetMode, setMode } from "mode-watcher";
 
     setMode("dark");
-
-    interface CompletionTokens {
-        tokens: string;
-    }
-
-    let lastCompletionTimestamp = 0;
-    listen<CompletionTokens>("completion-tokens", async (event) => {
-        if (!event.payload || !event.payload.tokens) {
-            return;
-        }
-        // Remove latest bot message
-        let latest = history.messages.pop();
-        if (!latest || latest.author !== "bot") {
-            return;
-        }
-        if (latest.content === "...") {
-            latest.content = "";
-        }
-        let content = latest.content + event.payload.tokens;
-        let html = latest.html;
-        let now = Date.now();
-        let diff = now - lastCompletionTimestamp;
-        if (diff > 10 || lastCompletionTimestamp === 0) {
-            html = toHighlightedHtml(content);
-            lastCompletionTimestamp = now;
-        }
-        history.messages.push({
-            author: "bot",
-            content,
-            html,
-            timestamp: new Date(),
-        });
-    });
-
-    async function send() {
-        let inputElement = document.querySelector(
-            'textarea[name="userMessage"]',
-        ) as HTMLInputElement;
-        if (!inputElement) return;
-        let message = inputElement.value;
-        if (!message) return;
-        history.messages.push({
-            author: "user",
-            content: message,
-            html: toHtml(message),
-            timestamp: new Date(),
-        });
-        inputElement.value = "";
-        let completion = "";
-        try {
-            let future: Promise<string> = invoke("complete_history", {
-                history,
-            });
-            history.messages.push({
-                author: "bot",
-                content: "...",
-                html: toHtml("..."),
-                timestamp: new Date(),
-            });
-            completion = await future;
-        } catch (error) {
-            completion =
-                "I'm sorry, I can't do that right now.: " + error + ".";
-        }
-        history.messages.pop();
-        history.messages.push({
-            author: "bot",
-            content: completion,
-            html: toHighlightedHtml(completion),
-            timestamp: new Date(),
-        });
-    }
-
-    interface Message {
-        author: "user" | "bot";
-        content: string;
-        html: string;
-        timestamp: Date;
-    }
-
-    interface History {
-        id: string;
-        messages: Message[];
-        initialIndex: number;
-    }
-
-    const initialContent =
-        'Hi, I\'m Alice, your personal assistant. I can help you with a lot of things.\n## Capibilities\n- Search for information on the web.\n- Help you with your homework.\n- Write stories.\n- Help you with your workout plan.\n- Write code.\n## Examples\n- Create a workout plan.\n- Help me with my math homework.\n- Write a story about a dragon.\n- Write a program that prints "Hello, World!".';
-    let history: History = $state({
-        id: "46695d58-a0d0-4e26-a457-f210bbb1106f",
-        initialIndex: 0,
-        messages: [
-            {
-                author: "bot",
-                content: initialContent,
-                html: toHtml(initialContent),
-                timestamp: new Date(),
-            },
-        ],
-    });
+    let chats = [
+        {
+            date_group: "Today",
+            conversations: [
+                {
+                    title: "Lorem ipsum dolor sit amet",
+                    id: "1",
+                },
+                {
+                    title: "Consectetur adipiscing",
+                    id: "2",
+                },
+                {
+                    title: "Sed do eiusmod tempor incididunt",
+                    id: "3",
+                },
+                {
+                    title: "Ut labore et dolore magna aliqua",
+                    id: "4",
+                },
+            ],
+        },
+        {
+            date_group: "Yesterday",
+            conversations: [
+                {
+                    title: "Ut enim ad minim",
+                    id: "5",
+                },
+                {
+                    title: "Quis nostrud exercitation ullamco",
+                    id: "6",
+                },
+                {
+                    title: "Laboris nisi ut aliquip ex ea commodo",
+                    id: "7",
+                },
+                {
+                    title: "Consequat duis",
+                    id: "8",
+                },
+            ],
+        },
+        {
+            date_group: "Last Week",
+            conversations: [
+                {
+                    title: "In reprehenderit in voluptate",
+                    id: "9",
+                },
+                {
+                    title: "Velit esse cillum dolore eu fugiat",
+                    id: "10",
+                },
+                {
+                    title: "Nulla pariatur",
+                    id: "11",
+                },
+                {
+                    title: "Excepteur sint occaecat cupidatat",
+                    id: "12",
+                },
+            ],
+        },
+        {
+            date_group: "Last Month",
+            conversations: [
+                {
+                    title: "Non proident sunt in culpa",
+                    id: "13",
+                },
+                {
+                    title: "Lorem ipsum dolor sit amet",
+                    id: "14",
+                },
+                {
+                    title: "Consectetur adipiscing",
+                    id: "15",
+                },
+                {
+                    title: "Sed do eiusmod tempor incididunt",
+                    id: "16",
+                },
+            ],
+        },
+        {
+            date_group: "3+ Months Ago",
+            conversations: [
+                {
+                    title: "Ut labore et dolore magna aliqua",
+                    id: "17",
+                },
+                {
+                    title: "Ut enim ad minim",
+                    id: "18",
+                },
+                {
+                    title: "Quis nostrud exercitation ullamco",
+                    id: "19",
+                },
+                {
+                    title: "Laboris nisi ut aliquip ex ea commodo",
+                    id: "20",
+                },
+            ],
+        },
+        {
+            date_group: "Older",
+            conversations: [
+                {
+                    title: "Consequat duis",
+                    id: "21",
+                },
+                {
+                    title: "In reprehenderit in voluptate",
+                    id: "22",
+                },
+                {
+                    title: "Velit esse cillum dolore eu fugiat",
+                    id: "23",
+                },
+                {
+                    title: "Nulla pariatur",
+                    id: "24",
+                },
+            ],
+        },
+    ];
 </script>
 
-<Sidebar />
-<main>
-    <div class="chat-box">
-        <div class="history">
-            {#each history.messages as message}
-                {#if message.author === "user"}
-                    <div class="user-message">
-                        <P>{message.content}</P>
+<div class="flex flex-col h-screen">
+    <!-- Sidebar Nav: Top has buttons for toggling sidebar and new chat, bottom has list of chats -->
+    <nav class="flex flex-col h-screen bg-gray-800 text-white max-w-[260px]">
+        <div class="flex justify-between items-center p-4">
+            <button class="text-white" on:click={resetMode}
+                >Toggle Sidebar</button
+            >
+            <button class="text-white">New Chat</button>
+        </div>
+        <!-- List of chats, text should aligned to the left, scrollable -->
+        <ScrollArea class="p-4">
+            {#each chats as chat}
+                <div class="date-group pb-4">
+                    <p2 class="font-bold text-white p-2">
+                        {chat.date_group}
+                    </p2> <br />
+                    <div class="flex flex-col">
+                        {#each chat.conversations as conv}
+                            <!-- fade mask on right side of text -->
+                            <div
+                                class="relative group flex flex-row justify-between chat-link hover:bg-gray-700 p-2 rounded-xl"
+                            >
+                                <a
+                                    class="text-gray-400 text-nowrap w-full lg:fade-out-mask group-hover:lg:fade-out-mask-earlier"
+                                    href="/?id={conv.id}"
+                                >
+                                    {conv.title}
+                                </a>
+                                <span
+                                    class="chat-options invisible group-hover:visible absolute right-2 align-middle"
+                                >
+                                    ⋯
+                                </span>
+                            </div>
+                        {/each}
                     </div>
-                {:else}
-                    <div class="bot-message">
-                        {@html message.html}
-                    </div>
-                {/if}
+                </div>
             {/each}
-        </div>
-        <div class="bottom-bar">
-            <ConnectionIndicator />
-            <Textarea
-                rows="1"
-                name="userMessage"
-                placeholder="Chat with Alice."
-            />
-            <Button onclick={send}>Send</Button>
-        </div>
-    </div>
-</main>
+        </ScrollArea>
+    </nav>
+
+    <!-- Main Content: Top is for some misc settings/controls, middle is chat area, bottom is for input -->
+    <main>
+        <div id="controls"></div>
+        <div id="chat"></div>
+        <div id="input"></div>
+    </main>
+</div>
 
 <style>
-    main {
-        display: flex;
-        height: 100svh;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
+    @tailwind utilities;
 
-    .chat-box {
-        display: flex;
-        flex-grow: 1;
-        height: 100%;
-        max-width: 1200px;
-        min-width: 700px;
-        flex-direction: column;
-        justify-content: space-between;
-    }
+    @layer utilities {
+        .fade-out-mask {
+            -webkit-mask-image: linear-gradient(
+                to right,
+                black 90%,
+                transparent 100%
+            );
+            mask-image: linear-gradient(to right, black 90%, transparent 100%);
+            -webkit-mask-repeat: no-repeat;
+            mask-repeat: no-repeat;
+            -webkit-mask-size: 100% 100%;
+            mask-size: 100% 100%;
+        }
 
-    .history {
-        overflow-y: auto;
-        display: flex;
-        flex-direction: column;
-        mask-image: linear-gradient(
-            to bottom,
-            transparent 0%,
-            black 30px,
-            black calc(100% - 30px),
-            transparent 100%
-        );
-        padding: 4rem;
-    }
-
-    .history::-webkit-scrollbar {
-        display: none;
-    }
-
-    .history :global(p) {
-        text-align: justify;
-        text-justify: inter-word;
-    }
-
-    .user-message {
-        background-color: var(--color-accent);
-        padding: 1rem;
-        margin: 1rem;
-        border-radius: 1rem;
-        color: var(--color-accent);
-        align-self: flex-end;
-    }
-
-    .bottom-bar {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        margin: 1rem 1rem 1rem 1rem;
-    }
-
-    .bottom-bar :global(textarea) {
-        width: 100%;
-        margin-left: 1rem;
-        margin-right: 1rem;
+        .fade-out-mask-earlier {
+            -webkit-mask-image: linear-gradient(
+                to right,
+                black 80%,
+                transparent 90%
+            );
+            mask-image: linear-gradient(to right, black 83%, transparent 93%);
+            -webkit-mask-repeat: no-repeat;
+            mask-repeat: no-repeat;
+            -webkit-mask-size: 100% 100%;
+            mask-size: 100% 100%;
+        }
     }
 </style>
