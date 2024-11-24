2f5419863c309ac541f407a078a1abd76631c876
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-24 15:17:20 +0100

    chore: added svelte-sonner and shadcn Sonner dependencies
    chore: update mode-watcher and add sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { Toaster } from "$lib/components/ui/sonner/index.js";
     import { ModeWatcher } from "mode-watcher";
     import '../app.css';
     let { children } = $props();
 </script>
 
+<Toaster />
 <ModeWatcher />
 {@render children()}
