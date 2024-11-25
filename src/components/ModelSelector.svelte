8e9601fd50ac4b1688401492964b3c49ddbc589e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-25 13:49:03 +0100

    feat: initial implementation of model loading and listing for frontend

--- src/components/ModelSelector.svelte
+++ src/components/ModelSelector.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import * as Select from "$lib/components/ui/select/index.js";
 
-    let selected_id = $state(null);
+    import { invoke } from "@tauri-apps/api/core";
+    import { listen } from "@tauri-apps/api/event";
 
-    $effect(() => {
-        if (selected_id) {
-            model = models.find((m) => m.id === selected_id) ?? null;
-        }
+    import { toast } from "svelte-sonner";
+    import { onMount } from "svelte";
+
+    let status = $state("unloaded");
+
+    onMount(async () => {
+        const rawModels: { engine: string; name: string }[] =
+            await invoke("list_models");
+        models = modelListToMapWithIndexId(rawModels);
     });
 
     let {
@@ -19,34 +25,43 @@
         } | undefined;
     } = $props();
 
-    let models = [
-        {
-            id: "1",
-            name: "Model 1",
-        },
-        {
-            id: "2",
-            name: "Model 2",
-        },
-        {
-            id: "3",
-            name: "Model 3",
-        },
-    ];
+    let models: {
+        id: string;
+        engine: string;
+        name: string;
+    }[] = $state([]);
 
-    function loadModel() {
-        // TODO: Load a model.
+    function modelListToMapWithIndexId(
+        models: { engine: string; name: string }[],
+    ): { id: string; engine: string; name: string }[] {
+        let map = [];
+        for (let i = 0; i < models.length; i++) {
+            map[i] = {
+                id: i.toString(),
+                ...models[i],
+            };
+        }
+        return map;
     }
 
-    function unloadModel() {
-        // TODO: Unload a model.
+    listen<string>("model_load", (event) => {
+        status = event.payload;
+        console.log("Model load event", status);
+        toast.info(`Model load event: ${status}`);
+    });
+
+    async function loadModel(value: string) {
+        if (status == "loading") return;
+        model = models.find((m) => m.id === value);
+        if (!model) return;
+        await invoke("load_model", { model });
     }
 </script>
 
 <Select.Root
     type="single"
-    disabled={models.length === 0}
-    bind:value={selected_id}
+    disabled={models.length === 0 || status === "loading"}
+    onValueChange={(value) => loadModel(value)}
 >
     <Select.Trigger class="max-w-[260px] truncate">
         {#if model}
