c953de786912ca79b9b35ee5349d94408406bc40
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-11-27 18:52:32 +0100

    feat: String into AliceError

--- src-tauri/src/prelude.rs
+++ src-tauri/src/prelude.rs
@@ -6,6 +6,11 @@ pub type Result<T> = std::result::Result<T, AliceError>;
 pub enum AliceError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
+
+    #[error("Other error: {0}")]
+    Other(String),
+
+    // Serde
     #[error("Serde error: {0}")]
     Serde(#[from] serde_json::Error),
 
@@ -35,5 +40,11 @@ pub enum AliceError {
 impl From<AliceError> for String {
     fn from(value: AliceError) -> Self {
         format!("{}", value)
+    }
+}
+
+impl From<&str> for AliceError {
+    fn from(value: &str) -> Self {
+        AliceError::Other(value.to_string())
     }
 }
